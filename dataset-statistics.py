8e1540a8a12b75f9486272ee8317e0a037808060
ericsargsyan <eric.sargsiann@gmail.com>
2023-01-14 21:26:16 +0400

    analyze

--- dataset-statistics.py
+++ dataset-statistics.py
@@ -1,4 +1,5 @@
 import os
+import numpy as np
 import pandas as pd
 import soundfile as sf
 import matplotlib.pyplot as plt
@@ -8,11 +9,12 @@ path = '/media/capstone/HDD2/source_data/MozillaCV/en/validated.tsv'
 en_data = pd.read_table(path)[['path']]
 
 durations = []
+bins = np.arange(0, 10, 0.3)
 
 for audio in tqdm(en_data['path']):
     data, samplerate = sf.read(os.path.join('/media/capstone/HDD2/source_data/MozillaCV/en/clips', f'{audio[:-3]}wav'))
     duration = data.shape[0] / samplerate
     durations.append(duration)
 
-plt.hist(durations)
-plt.savefig('en_hist.png')
+plt.hist(durations, bins=bins)
+plt.savefig('en_hist_normal.png')
