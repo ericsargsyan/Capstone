69fc61e1ec14eddf403be39840e67d388dcabc3b
ericsargsyan <eric.sargsiann@gmail.com>
2023-01-15 15:10:15 +0400

    changes in language

--- dataflow/importers/language/MozillaCV.py
+++ dataflow/importers/language/MozillaCV.py
@@ -23,25 +23,29 @@ class MozillaCVLImporter:
             print(f'------------------------------------------------------------------------'
                   f'    Processing {language.upper()} Data    '
                   f'------------------------------------------------------------------------')
-            dataset = pd.read_table(os.path.join(self.source_path, language, 'validated.tsv'))['path']
-            train_val, test = np.split(dataset.sample(frac=1, random_state=12, replace=False), [int(len(dataset)*0.8)])
-            test = test.reset_index()
-            train, val = np.split(train_val.sample(frac=1, random_state=12, replace=False), [int(len(train_val)*0.9)])
-            train, val = train.reset_index(), val.reset_index()
+            test_dataset = pd.read_table(os.path.join(self.source_path, language, 'test.tsv'))[['path']]
+            dev_dataset = pd.read_table(os.path.join(self.source_path, language, 'dev.tsv'))[['path']]
+            validated_dataset = pd.read_table(os.path.join(self.source_path, language, 'validated.tsv'))[['path']]
 
-            self.process_data(train, language, 'TRAIN')
-            self.process_data(val, language, 'VAL')
-            self.process_data(test, language, 'TEST')
+            training_data = set(validated_dataset.path.to_numpy()) - set(test_dataset.path.unique()) - set(dev_dataset.path.unique())
+            train_dataset = pd.DataFrame(training_data, columns=['path'])
+
+            self.process_data(test_dataset, language, 'TEST')
+            self.process_data(dev_dataset, language, 'DEV')
+            self.process_data(train_dataset, language, 'TRAIN')
 
     def process_data(self, audios, language, split):
+        print(f'------------------------------------------------------------------------'
+              f'Processing {split.upper()} Data'
+              f'------------------------------------------------------------------------')
         os.makedirs(os.path.join(self.target_dir, language, split), exist_ok=True)
         index = 0
 
         for file_name in tqdm(audios['path']):
-            current_path = os.path.join(self.source_path, language, 'clips', file_name)
+            current_path = os.path.join(self.source_path, language, 'clips', f'{file_name[:-3]}wav')
             data = format_audio(current_path, self.samplerate, self.duration)
 
-            new_audio_filepath = os.path.join(self.target_dir, language, split, f'{file_name}.wav')
+            new_audio_filepath = os.path.join(self.target_dir, language, split, f'{file_name[:-3]}wav')
             sf.write(new_audio_filepath, data, self.samplerate)
 
             labels_path = os.path.join(self.csv_path, f'{split}_mozilla.csv')
