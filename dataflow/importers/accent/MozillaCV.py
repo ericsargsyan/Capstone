b5a7bbe720fbf2ca42e098baf3bec6fe9d6496ab
ericsargsyan <eric.sargsiann@gmail.com>
2023-01-15 15:10:49 +0400

    changes in accent

--- dataflow/importers/accent/MozillaCV.py
+++ dataflow/importers/accent/MozillaCV.py
@@ -18,27 +18,35 @@ class MozillaCVAImporter:
         self.duration = config['duration']
 
     def import_dataset(self):
-        dataset = pd.read_table(os.path.join(self.source_path, 'en', 'validated.tsv'))[['path', 'accents']]
-        dataset = dataset[dataset['accents'].notnull()]
-        dataset = dataset.reset_index()
-        train_val, test = np.split(dataset.sample(frac=1, random_state=12, replace=False), [int(len(dataset) * 0.8)])
-        train, val = np.split(train_val.sample(frac=1, random_state=12, replace=False), [int(len(train_val) * 0.9)])
-        train, val, test = train.reset_index(), val.reset_index(), test.reset_index()
+        test_dataset = pd.read_table(os.path.join(self.source_path, 'en', 'test.tsv'))[['path', 'accents']]
+        dev_dataset = pd.read_table(os.path.join(self.source_path, 'en', 'dev.tsv'))[['path', 'accents']]
+        validated_dataset = pd.read_table(os.path.join(self.source_path, 'en', 'validated.tsv'))[['path', 'accents']]
+        test_dataset = test_dataset[test_dataset['accents'].notnull()]
+        dev_dataset = dev_dataset[dev_dataset['accents'].notnull()]
+        validated_dataset = validated_dataset[validated_dataset['accents'].notnull()]
 
-        self.process_data(train, 'TRAIN')
-        self.process_data(val, 'VAL')
-        self.process_data(test, 'TEST')
+        # training_data = set(validated_dataset.path.to_numpy()) - set(test_dataset.path.unique()) - set(dev_dataset.path.unique())
+        # train_dataset = pd.DataFrame(training_data, columns=['path', 'accents'])
+
+        print(set(validated_dataset))
+
+        self.process_data(test_dataset, 'TEST')
+        self.process_data(dev_dataset, 'DEV')
+        # self.process_data(train_dataset, 'TRAIN')
 
     def process_data(self, audios, split):
+        print(f'------------------------------------------------------------------------'
+              f'Processing {split.upper()} Data'
+              f'------------------------------------------------------------------------')
         os.makedirs(os.path.join(self.target_dir, split), exist_ok=True)
         index = 0
 
         for file_name in tqdm(audios['path']):
-            current_path = os.path.join(self.source_path, 'en', 'clips', file_name)
+            current_path = os.path.join(self.source_path, 'en', 'clips', f'{file_name[:-3]}wav')
             data = format_audio(current_path, self.samplerate, self.duration)
             accent = audios.loc[audios['path'] == file_name]['accents'][index]
 
-            new_audio_filepath = os.path.join(self.target_dir, split, f'{file_name}.wav')
+            new_audio_filepath = os.path.join(self.target_dir, split, f'{file_name[:-3]}wav')
             sf.write(new_audio_filepath, data, self.samplerate)
 
             labels_path = os.path.join(self.csv_path, f'{split}_mozilla.csv')
