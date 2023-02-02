868aad76c5bbc1b6f3b4f7d24fd95cc92d7c78ff
ericsargsyan <eric.sargsiann@gmail.com>
2023-02-02 10:36:39 +0400

    utils

--- dataflow/utils.py
+++ dataflow/utils.py
@@ -13,16 +13,23 @@ def read_yaml(path):
         return config
 
 
-def format_audio(current_path, self_samplerate, self_duration):
-    data, samplerate = sf.read(current_path)
-    # data = librosa.resample(data_raw, target_sr=self_samplerate, orig_sr=samplerate)
+def format_audio(current_path, self_samplerate, self_duration, resample):
+    if resample:
+        data_raw, samplerate = sf.read(current_path)
+        data = librosa.resample(data_raw, target_sr=self_samplerate, orig_sr=samplerate)
+    else:
+        data, samplerate = sf.read(current_path)
+
     duration = data.shape[0] / self_samplerate
 
     if duration > self_duration:
         data = data[:self_samplerate * self_duration]
     else:
         diff = self_duration * self_samplerate - data.shape[0]
-        data = np.pad(data, pad_width=(0, diff))
+
+        probability = np.random.rand()
+        p, p_1 = int(diff * probability), diff - int(diff * probability)
+        data = np.pad(data, pad_width=(p, p_1))
 
     return data
 
@@ -30,12 +37,15 @@ def format_audio(current_path, self_samplerate, self_duration):
 def mp3_to_wav(languages, path):
     for language in languages:
         dataset = pd.read_table(os.path.join(path, language, 'validated.tsv'))
+        os.makedirs(os.path.join(path, language, 'wav_clips'), exist_ok=True)
+        partial_path = os.path.join(path, language)
+
         for audio_path in tqdm(dataset['path']):
             out_path = audio_path[:-3]
             audio_to_convert = os.path.join(path, language, 'clips', audio_path)
             output_full_path = os.path.join(path, language, 'wav_clips', out_path)
-            command = f"ffmpeg -i {audio_to_convert} -acodec pcm_s16le -ac 1 -ar 16000 {output_full_path}wav"
+            command = f"ffmpeg -y -hwaccel cuda -i {audio_to_convert} -acodec pcm_s16le -ac 1 -ar 16000 {output_full_path}wav"
             os.system(command)
-        # os.system(f'rm -r {language}')
-        os.system(f'mv -T {os.path.join(path, language, "clips")} old_clips')
-        os.system(f'mv -T {os.path.join(path, language, "wav_clips")} clips')
+
+        os.system(f'mv {os.path.join(partial_path, "clips")} {os.path.join(partial_path, "old_clips")}')
+        os.system(f'mv {os.path.join(partial_path, "wav_clips")} {os.path.join(partial_path, "clips")}')
