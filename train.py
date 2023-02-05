3711ebf7c234f5d7cca3626270e32d286b52db59
ericsargsyan <eric.sargsiann@gmail.com>
2023-02-05 18:27:52 +0400

    training file

---
+++ train.py
@@ -0,0 +1,39 @@
+import os
+import argparse
+from dataflow.utils import read_yaml
+from dataset import AccentDataset, LanguageDataset
+from model import AudioModel
+from torch.utils.data import DataLoader
+from pytorch_lightning import Trainer
+from pytorch_lightning.loggers import TensorBoardLogger
+from pytorch_lightning.callbacks import ModelCheckpoint
+
+
+def arg_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--config_path',
+                        type=str,
+                        required=True)
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    parser = arg_parser()
+    config = read_yaml(parser.config_path)
+    dataloader_config = config['dataloader']
+
+    train_path = config['data']['train_path']
+    val_path = config['data']['val_path']
+
+    train_dataset = LanguageDataset(train_path)
+    val_dataset = LanguageDataset(val_path)
+
+    train_dataloader = DataLoader(train_dataset, batch_size=dataloader_config['batch_size'],
+                                                 shuffle=True, num_workers=config['dataloader']['num_workers'])
+    val_dataloader = DataLoader(val_dataset, batch_size=dataloader_config['batch_size'],
+                                             shuffle=False, num_workers=config['dataloader']['num_workers'])
+
+    model = AudioModel(7, config['learning_rate'])
+
+    trainer = Trainer()
+    trainer.fit(model, train_dataloader, val_dataloader)
