4640e7f2958df742c13a7661c077f8ddca3ca78e
ericsargsyan <eric.sargsiann@gmail.com>
2023-02-06 17:11:32 +0400

    Audio/Language Torch dataset

--- dataset.py
+++ dataset.py
@@ -1,5 +1,9 @@
 from torch.utils.data import Dataset
 import pandas as pd
+import torchaudio
+import torch
+import os
+from sklearn.preprocessing import LabelEncoder
 
 
 class AccentDataset(Dataset):
@@ -14,12 +18,19 @@ class AccentDataset(Dataset):
 
 
 class LanguageDataset(Dataset):
-    def __init__(self, paths):
-        self.data = pd.concat((pd.read_csv(data) for data in paths), ignore_index=True)
+    def __init__(self, path):
+        self.data = pd.read_csv(path)
+        self.data['language'] = self.data['language'].astype('category')
+        self.data['encoded_language'] = self.data['language'].cat.codes
+        dummy_data = self.data[['language', 'encoded_language']].drop_duplicates().reset_index(drop=True)
+        dummy_data.to_csv(os.path.join(os.getcwd(), 'encoded_labels.csv'))
 
     def __getitem__(self, idx):
-        # pass
-        pass
+        waveform, _ = torchaudio.load(self.data['path'][idx])
+        x = waveform.view(-1)
+        y = torch.tensor(self.data['encoded_language'], dtype=torch.float32)
+
+        return x, y[idx].unsqueeze(-1)
 
     def __len__(self):
         return len(self.data)
