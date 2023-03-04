f35c581306de75ed3633602364e542973c84fc3e
ericsargsyan <eric.sargsiann@gmail.com>
2023-03-04 17:30:57 +0400

    net

---
+++ net/layers.py
@@ -0,0 +1,78 @@
+import torch
+from torch import nn
+
+
+class TSConv(nn.Module):
+    def __init__(self, input_size, kernel_size, output_size, relu=True):
+        super().__init__()
+        self.func = nn.Sequential(
+            nn.Conv1d(in_channels=input_size,
+                      out_channels=input_size,
+                      kernel_size=kernel_size,
+                      groups=input_size,
+                      padding="same"),
+            nn.Conv1d(input_size, output_size, kernel_size=1),
+            nn.BatchNorm1d(output_size)
+        )
+        self.do_relu = relu
+
+    def forward(self, x):
+        if self.do_relu:
+            return nn.functional.relu(self.func(x))
+        return self.func(x)
+
+# x = torch.randn(165*40, 165*40)
+# conv = nn.Conv1d(10, 8, 2)
+# print(conv(x))
+# model = TSconv(165*40, 3, 165*40)
+# print(model.forward(x).size())
+
+
+class BBlock(nn.Module):
+    def __init__(self, input_size, kernel_size, output_size, repeat):
+        super().__init__()
+        self.model = nn.ModuleList([TSConv(input_size, kernel_size, output_size)])
+        for i in range(repeat-2):
+            self.model.append(TSConv(output_size, kernel_size, output_size))
+        self.model.append(TSConv(output_size, kernel_size, output_size, relu=False))
+
+        self.repeat = repeat
+        self.pointwise = nn.Conv1d(input_size, output_size, kernel_size=1)
+        self.batchnorm = nn.BatchNorm1d(output_size)
+        self.relu = nn.ReLU()
+
+    def forward(self, x):
+        x1 = x
+        for i in range(self.repeat):
+            x1 = self.model[i](x1)
+
+        x2 = self.batchnorm(self.pointwise(x))
+        return self.relu(x1 + x2)
+
+
+# x = torch.rand((165*40, 165*40))
+# model = BBlock(165*40, 3, 165*40, 3)
+# print(model.forward(x))
+
+class ConvBatchNormRelu(nn.Module):
+    def __init__(self, input_size, kernel_size, output_size):
+        super().__init__()
+        self.func = nn.Sequential(
+            nn.Conv1d(input_size, output_size, kernel_size=kernel_size),
+            nn.BatchNorm1d(output_size)
+        )
+
+    def forward(self, x):
+        return self.func(x)
+
+
+class StatsPooling(nn.Module):
+    def forward(self, x):
+        x = torch.cat([torch.mean(x, dim=-1), torch.std(x, dim=-1)], dim=1)
+        return x
+
+
+name_to_layer = nn.__dict__
+name_to_layer["StatsPooling"] = StatsPooling
+name_to_layer["ConvBatchNormRelu"] = ConvBatchNormRelu
+name_to_layer["BBlock"] = BBlock
---
+++ net/speaker_net.py
@@ -0,0 +1,21 @@
+from net.layers import *
+from torchaudio.transforms import MFCC
+
+
+class SpeakerNet(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.mfcc = MFCC()
+        self.layers = nn.ModuleList()
+
+        for layer_info in config:
+            layer_class = name_to_layer[layer_info['name']]
+            current_layer = layer_class(**layer_info['params'])
+            self.layers.append(current_layer)
+
+    def forward(self, x):
+        x = self.mfcc(x)
+        for layer in self.layers:
+            x = layer(x)
+        return x
