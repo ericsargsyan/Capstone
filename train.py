004b33fd444b8e6feb49c0f7bde6682bfe03a736
ericsargsyan <eric.sargsiann@gmail.com>
2023-03-25 17:16:08 +0400

    Adding TensorBoardLogger, ModelCheckpoint, update requirements.txt

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ from dataset import AccentDataset, LanguageDataset
 from model import AudioModel
 from torch.utils.data import DataLoader
 from pytorch_lightning import Trainer
+from utils import get_last_version_number
 from pytorch_lightning.loggers import TensorBoardLogger
 from pytorch_lightning.callbacks import ModelCheckpoint
 
@@ -37,5 +38,16 @@ if __name__ == "__main__":
 
     model = AudioModel(model_config, config['audio_processor'],  config['sr'], 7, config['learning_rate'])
 
-    trainer = Trainer(**config['pl_trainer'])
+    log_dir_path = config['log_dir'][config['task']]
+    version_number = get_last_version_number(log_dir_path)
+    logger = TensorBoardLogger(os.path.join(log_dir_path, version_number), name='', version='')
+    checkpoints_dir = os.path.join(log_dir_path, version_number, 'checkpoints')
+
+    checkpoint_callback = ModelCheckpoint(save_top_k=5,
+                                          filename="{epoch:02d}-{val_epoch_accuracy:.6f}",
+                                          dirpath=checkpoints_dir,
+                                          monitor='val_epoch_accuracy')
+
+    trainer = Trainer(callbacks=[checkpoint_callback], logger=logger, **config['pl_trainer'])
+    # trainer = Trainer(**config['pl_trainer'])
     trainer.fit(model, train_dataloader, val_dataloader)
