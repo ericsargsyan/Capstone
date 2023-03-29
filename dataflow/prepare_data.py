15e42eb9a922fa2c8f0e8447e2ad7b176e38bb10
Gor Piliposyan <gorpiliposyan@Gors-MacBook-Pro.local>
2023-03-29 22:34:52 +0400

    Prepare_data

--- dataflow/prepare_data.py
+++ dataflow/prepare_data.py
@@ -3,6 +3,7 @@ import argparse
 from dataflow.utils import read_yaml
 from dataflow.utils import mp3_to_wav
 from dataflow.importers.mozilla import LanguageImporter, AccentImporter
+from dataflow.importers.musan import MusanImporter
 
 
 def arg_parser():
@@ -14,9 +15,11 @@ def arg_parser():
 
 
 name_to_class = {'language_detection':
-                            {'MozillaCV': LanguageImporter},
+                            {'MozillaCV': LanguageImporter,
+                             'Musan': MusanImporter},
                  'accent_detection':
-                            {'MozillaCV': AccentImporter}
+                            {'MozillaCV': AccentImporter,
+                             'Musan': MusanImporter}
                  }
 
 if __name__ == '__main__':
@@ -27,12 +30,10 @@ if __name__ == '__main__':
         for convert_data in config['convert']['datasets_to_convert']:
             mp3_to_wav(config['languages'], config['datasets'][convert_data]['source_path'])
 
-    exit()
+    # for task in config['task']:
+    #     print(f"{'-' * 65}| Processing {task.title()} Data |{'-' * 65}")
+    #     os.makedirs(os.path.join(config['target_dir'], task), exist_ok=True)
 
-    for task in config['task']:
-        print(f"{'-' * 65}| Processing {task.title()} Data |{'-' * 65}")
-        os.makedirs(os.path.join(config['target_dir'], task), exist_ok=True)
-
-        for dataset_name in config['datasets_to_process']:
-            dataset_importer = name_to_class[task][dataset_name](config)
-            dataset_importer.import_dataset()
+    for dataset_name in config['datasets_to_process']:
+        dataset_importer = name_to_class[config['task']][dataset_name](config)
+        dataset_importer.import_dataset()
