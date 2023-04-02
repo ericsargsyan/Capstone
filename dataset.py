2f16dba5c04592389a677827520377596916ffea
ericsargsyan <eric.sargsiann@gmail.com>
2023-04-02 18:49:47 +0400

    Torch AudioDataset

--- dataset.py
+++ dataset.py
@@ -4,26 +4,15 @@ import torchaudio
 import torch
 
 
-class AccentDataset(Dataset):
-    def __init__(self, paths, label):
+class AudioDataset(Dataset):
+    def __init__(self, paths, label, task):
+        print('1')
         self.data = pd.concat((pd.read_csv(data) for data in paths), ignore_index=True)
+        print('2')
         self.labels = pd.DataFrame.from_dict(label, orient='index', columns=['label'])
-        self.data = pd.merge(self.data, self.labels, left_on='accent', right_index=True)
-
-    def __getitem__(self, idx):
-        waveform, _ = torchaudio.load(self.data['path'][idx])
-        x = waveform.view(-1)
-        y = torch.tensor(self.data['label'], dtype=torch.long)
-
-    def __len__(self):
-        return len(self.data)
-
-
-class LanguageDataset(Dataset):
-    def __init__(self, path, label):
-        self.data = pd.read_csv(path)
-        self.labels = pd.DataFrame.from_dict(label, orient='index', columns=['label'])
-        self.data = pd.merge(self.data, self.labels, left_on='language', right_index=True)
+        print('3')
+        self.data = pd.merge(self.data, self.labels, left_on=task.split('_')[0], right_index=True)
+        print('4')
 
     def __getitem__(self, idx):
         waveform, _ = torchaudio.load(self.data['path'][idx])
