5ae4e4845bfd3f1f0ecb2d78322ffeacb912da9f
ericsargsyan <eric.sargsiann@gmail.com>
2023-04-04 16:53:53 +0400

    Torch Audio Dataset for Accent and language

--- dataset.py
+++ dataset.py
@@ -6,13 +6,9 @@ import torch
 
 class AudioDataset(Dataset):
     def __init__(self, paths, label, task):
-        print('1')
         self.data = pd.concat((pd.read_csv(data) for data in paths), ignore_index=True)
-        print('2')
         self.labels = pd.DataFrame.from_dict(label, orient='index', columns=['label'])
-        print('3')
         self.data = pd.merge(self.data, self.labels, left_on=task.split('_')[0], right_index=True)
-        print('4')
 
     def __getitem__(self, idx):
         waveform, _ = torchaudio.load(self.data['path'][idx])
