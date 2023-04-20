bf8fb8b15b789f74f7f787cc72eddac2b6c6f35e
ericsargsyan <eric.sargsiann@gmail.com>
2023-04-20 12:11:10 +0400

    Inference Part

--- infer.py
+++ infer.py
@@ -18,25 +18,24 @@ def arg_parser():
     parser.add_argument('--model_config_path',
                         type=str,
                         required=True)
-    # parser.add_argument('--check_path',
-    #                     type=str,
-    #                     required=True)
+    parser.add_argument('--check_path',
+                        type=str,
+                        required=True)
     parser.add_argument('--audios_dir',
                         type=str,
-                        # nargs='+',
                         required=True)
     return parser.parse_args()
 
 
-def detect_spoken_language_or_accent(audio, check_path, encodings):
+def detect_spoken_language_or_accent(data, encodings, check_path, model_config, processor_config, sr, labels, lr):
     audio_model = AudioModel.load_from_checkpoint(checkpoint_path=check_path,
-                                                  model_config=read_yaml('./configs/model/net.yaml'),
-                                                  processor_config=model_config['audio_processor'],
-                                                  sr=model_config['sr'],
-                                                  number_of_labels=model_config['encodings'][model_config['task']],
-                                                  learning_rate=model_config['learning_rate'])
+                                                  model_config=read_yaml(model_config),
+                                                  processor_config=processor_config,
+                                                  sr=sr,
+                                                  number_of_labels=labels,
+                                                  learning_rate=lr)
     audio_model.eval()
-    y_prob = audio_model(audio)
+    y_prob = audio_model(data)
 
     label = list(filter(lambda x: encodings[x] == torch.argmax(y_prob, dim=1), encodings))[0]
 
@@ -49,7 +48,12 @@ if __name__ == "__main__":
     model_config = read_yaml(parser.model_config_path)
     audio_dir = parser.audios_dir
 
+    task = model_config['task']
+    number_of_labels = max(model_config['encodings'][task].values()) + 1
+    encodings = model_config['encodings'][task]
+
     audios = []
+    audio_names = []
 
     for audio_path in glob(f'{audio_dir}{os.sep}*.mp3'):
         data = format_audio(audio_path,
@@ -58,14 +62,15 @@ if __name__ == "__main__":
                             self_duration=dataflow_config['duration'])
         data = torch.tensor(data, dtype=torch.float32).view(1, -1)
         audios.append(data)
+        audio_names.append(os.path.basename(audio_path)[:-4])
 
     data = torch.cat(audios, dim=0)
-    print(len(audios), audios[0].shape)
-    print(data.shape)
 
-    checkpoint_path = '/Users/eric/Desktop/Krisp/Capstone/language_logs/version_3/checkpoints/epoch=01-val_epoch_accuracy=0.930701.ckpt'
+    check_abs_path = os.path.join(model_config['log_dir'][task], parser.check_path, 'checkpoints')
+    checkpoint_files = glob(os.path.join(check_abs_path, '*.ckpt'))
 
-    number_of_labels = max(model_config['encodings'][model_config['task']].values()) + 1
+    accuracies = [float(checkpoint.split('=')[2][:-5]) for checkpoint in checkpoint_files]
+    checkpoint_path = checkpoint_files[accuracies.index(max(accuracies))]
 
     model = AudioModel.load_from_checkpoint(checkpoint_path=checkpoint_path,
                                             model_config=read_yaml('./configs/model/net.yaml'),
@@ -76,14 +81,8 @@ if __name__ == "__main__":
     model.eval()
     y_prob = model(data)
 
-    print(data.shape, y_prob)
+    speech = task.split('_')[0]
 
-    # print(y_prob)
-
-    # for idx, y in enumerate(y_prob):
-    #     if y >= 0.5:
-    #         print(f'Label of {audio_paths[idx].split(os.sep)[-1]} - M')
-    #     else:
-    #         print(f'Label of {audio_paths[idx].split(os.sep)[-1]} - F')
-
-
+    for index, y in enumerate(torch.argmax(y_prob, dim=1)):
+        label = list(filter(lambda x: encodings[x] == y, encodings))[0]
+        print(f'Spoken {speech} of {audio_names[index]}: {label}')
