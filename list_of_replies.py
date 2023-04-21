95632737258941e8142751bfab50367475a5e6b4
ericsargsyan <eric.sargsiann@gmail.com>
2023-04-22 00:09:53 +0400

    Telegram replies

---
+++ list_of_replies.py
@@ -0,0 +1,67 @@
+languages = {'en': 'Change Language 🌐',
+             'es': 'Cambiar Idioma 🌐',
+             'fr': 'Changer de Langue 🌐',
+             'hy': 'Փոխել Լեզուն 🌐'}
+
+back_texts = {'en': 'Back 🔙',
+              'es': 'Atrás 🔙',
+              'fr': 'Retour 🔙',
+              'hy': 'Ետ 🔙'}
+
+about = {'en': 'About Me 💬',
+         'es': 'Sobre Mi 💬',
+         'fr': 'Sur Moi 💬',
+         'hy': 'Իմ մասին 💬'}
+
+change_language = {'en': 'Please select your language',
+                   'es': 'Por favor seleccione su idioma',
+                   'fr': 'Veuillez sélectionner votre langue',
+                   'hy': 'Խնդրում ենք ընտրել ձեր լեզուն'}
+
+handle_messages = {'en': "Sorry, I didn't understand your message.😕 "
+                         "If you need help, please type /help to see a list of available commands",
+                   'es': 'Lo siento, no entendí tu mensaje.😕 '
+                         'Si necesita ayuda, escriba /ayuda para ver una lista de los comandos disponibles.',
+                   'fr': "Désolé, je n'ai pas compris votre message.😕 "
+                         "Si vous avez besoin d'aide, "
+                         "veuillez taper /help pour voir une liste des commandes disponibles.",
+                   'hy': 'Կներեք, ես չհասկացա ձեր հաղորդագրությունը:😕 '
+                         'Եթե օգնության կարիք ունեք, '
+                         'խնդրում ենք մուտքագրել /help՝ հասանելի հրամանների ցանկը տեսնելու համար:'}
+
+intro = {'en': "Let me introduce myself.\n"
+               "I am a Telegram Bot created by the students of the Faculty of Mathematics and Mechanics at YSU,"
+               "Eric Sargsyan and Gor Piliposyan."
+               "I am capable of detecting the language and accent from human speech.",
+         'es': "Permíteme contarte sobre mí:\n"
+               "Soy un bot de Telegram desarrollado por los estudiantes de la Facultad de Matemáticas y Mecánica "
+               "de la Universidad Estatal de Ereván Eric Sargsyan y Gor Piliposyan. "
+               "Puedo detectar el idioma y el vocabulario a partir del habla humana.",
+         'fr': "Permettez-moi de vous parler un peu de moi.\n "
+               "je suis Telegram Bot, qui a été créé par Erik Sargsyan et Gor Piliposyan, "
+               "étudiants de la Faculté de mathématiques et de mécanique de l'YSU. "
+               "Je peux reconnaître la langue et le dialecte à partir de la parole humaine.",
+         'hy': 'Միքիչ պատմեմ իմ մասին:\n'
+               'Ես Telegram Bot եմ, որը ստեծվել է ԵՊՀ Մաթեմատիկայի և մեխանիկայի ֆակուլտետի ուսանողներ '
+               'Էրիկ Սարգսյանի և Գոռ Փիլիպոսյանի կողմից։ Ես կարողանում եմ ճանաչել լեզուն և բարբառը մարդու խոսքից։'}
+
+voice_received = {'en': 'Voice message received. 🎤',
+                  'es': 'Mensaje de voz recibido. 🎤',
+                  'fr': "Message vocal reçu. 🎤",
+                  'hy': 'Ձայնային հաղորդագրություն ստացվել է:🎤'},
+
+help_text = {'en': "Here are the commands list. 🤖\n\n"
+                   "/start - Start the bot\n"
+                   "/support - How to control me\n"
+                   "/languages - Trained languages\n"
+                   "/accents - Trained accents\n"
+                   "/help - Get help\n",
+             'es': '',
+             'fr': '',
+             'hy': 'Ահա հրամանների ցանկը: 🤖'
+             }
+
+trained_languages = {'en': 'English, French, German, Spanish, Catalan, Belarusian',
+                     'es': 'Inglés, Francés, Alemán, Español, Catalán, Bielorruso',
+                     'fr': 'Anglais, Français, Allemand, Espagnol, Catalan, Biélorusse',
+                     'hy': 'Անգլերեն, Ֆրանսերեն, Գերմաներեն, Իսպաներեն, Կատալոներեն, Բելառուսերեն'}
