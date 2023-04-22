1c7f53fd5260b05b6e284f61122b46b8449c42fb
ericsargsyan <eric.sargsiann@gmail.com>
2023-04-23 00:27:13 +0400

    essential utils

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import os
+from glob import glob
 
 
 def ogg_to_wav(input_file, output_file):
@@ -12,8 +13,21 @@ def get_last_version_number(log_dir):
 
     versions = os.listdir(log_dir)
     versions = [version for version in versions if version[0] != '.']
+
     if len(versions) == 0:
         return "version_0"
+
     indexes = [int(version.split('_')[1]) for version in versions]
     max_version_index = max(indexes)
+
     return f'version_{max_version_index+1}'
+
+
+def get_best_checkpoint(base_path, check_path):
+    check_abs_path = os.path.join(base_path, check_path, 'checkpoints')
+    checkpoint_files = glob(os.path.join(check_abs_path, '*.ckpt'))
+
+    accuracies = [float(checkpoint.split('=')[2][:-5]) for checkpoint in checkpoint_files]
+    checkpoint_path = checkpoint_files[accuracies.index(max(accuracies))]
+
+    return checkpoint_path
